9c55cca1b928de1f9fe477f2d00ac38ec9fe12f9
QuangDoo <do.quang039@gmail.com>
2025-04-17 15:25:23 +0700

    chore: update .gitignore to include environment files and refactor vite.config.ts to load environment variables based on mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
       },
     },
-  },
-  define: {
-    "process.env": process.env,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    define: {
+      // Expose env variables to your app
+      __APP_ENV__: env.APP_ENV,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: parseInt(env.PORT || "3002"),
+    },
+  };
 });
